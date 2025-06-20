fd742dead5579d6924b6b823aa836eb1f6501401
Amanbanti <amanbanti2011@gmail.com>
2025-06-20 19:10:44 +0300

    ThemeProvider commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { 
+  SignedOut, 
+  SignInButton,
+  SignedIn,
+  UserButton
+ } from "@clerk/nextjs";
+
+ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <SignedOut>
+        <SignInButton mode="modal">{/*modlal-to make it popUp (Not rediract) */}
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
