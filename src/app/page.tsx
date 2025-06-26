5d8ad8d6aeef6e9937ede22110fa477925bc5900
Amanbanti <amanbanti2011@gmail.com>
2025-06-26 09:25:17 +0300

    ModelToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,12 @@ import {
   UserButton
  } from "@clerk/nextjs";
 
- import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">{/*modlal-to make it popUp (Not rediract) */}
-          <Button>
-            Sign In
-          </Button>
-        </SignInButton>
-
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      home page
     </div>
   );
 }
